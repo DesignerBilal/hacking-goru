413fb2b8ce02285ca619de1850b608b89fc112d4
Muhammad Bilal <165760679+DesignerBilal@users.noreply.github.com>
2024-06-04 14:27:39 +0500

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//Nav Bar
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+  }
